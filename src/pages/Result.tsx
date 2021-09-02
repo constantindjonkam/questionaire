ccd807bd986b7b09051884d4350d36db00791e3a
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-09-02 02:51:06 -0400

    populated result component with db data, created color and severity Determiner, added some text and result cards

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,9 +1,68 @@
-export interface ResultProps {}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../components/Header";
 
-const Result: React.FC<ResultProps> = () => {
+import "./styles/result.scss";
+import LineChart from "../components/LineChart";
+import { getUserResults, UserResult } from "../firebase";
+import ResultCard from "../components/ResultCard";
+import hypertension from "../assets/hypertension.jpg";
+import cognition from "../assets/cognition.jpg";
+import instrusions from "../assets/instrusions.jpg";
+import avoidance from "../assets/avoidance.jpg";
+
+const Result: React.FC = () => {
+  const [data, setData] = useState({} as UserResult);
+  const params = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const res = await getUserResults((params as { id: string }).id);
+      setData(res[0]);
+    })();
+  }, [params]);
+
+  const colorDeterminer = () => {
+    if (data.total > 50) return "red";
+    else if (data.total > 25) return "blue";
+    return "green";
+  };
+
+  const severityDeterminer = () => {
+    if (data.total > 50) return "High";
+    else if (data.total > 25) return "Medium";
+    return "Low";
+  };
+
+  // const resultDeterminer = () => {
+  //   if (data.total > 50) return "High";
+  //   else if (data.total > 25) return "Medium";
+  //   return "Low";
+  // };
+
   return (
     <div className="result">
-      <p>Result</p>
+      <Header title="Latest Result" />
+      <p>Base on the data collected we were able to determine the following scores:</p>
+      <div className="result__grid">
+        <ResultCard title="Avoidance" image={avoidance} value={data.avoidance} />
+        <ResultCard title="Intrusions" image={instrusions} value={data.intrusions} />
+        <ResultCard title="Cognitions" image={cognition} value={data.cognitions} />
+        <ResultCard title="Hypervigilance" image={hypertension} value={data.hypervigilance} />
+      </div>
+      <div className="result__total">
+        <p>
+          Total:
+          <span style={{ color: colorDeterminer() }}>{data.total}</span>
+        </p>
+        <p>
+          Health Severity: <span style={{ color: colorDeterminer() }}>{severityDeterminer()}</span>
+        </p>
+      </div>
+      <p>
+        You got an avoidance score of {data.avoidance}. This means you have higher chances of...
+      </p>
+      <LineChart />
     </div>
   );
 };
