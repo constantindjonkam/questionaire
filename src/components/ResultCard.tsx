6b6c5eb254fcff131477ce6b875b878985681359
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-09-02 15:59:57 -0400

    implemented result color determiner

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -7,12 +7,20 @@ export interface ResultCardProps {
 }
 
 const ResultCard: React.FC<ResultCardProps> = ({ image, title, value }) => {
+  const resultColorDeterminer = () => {
+    if (value >= 10) return { color: "white", backgroundColor: "red" };
+    if (value >= 5) return { color: "black", backgroundColor: "yellow" };
+    return { color: "white", backgroundColor: "green" };
+  };
+
   return (
     <div className="resultCard">
       <img src={image} alt={title} />
       <div>
         <p>{title}</p>
-        <p className="resultCard__value">{value}</p>
+        <p className="resultCard__value" style={resultColorDeterminer()}>
+          {value}
+        </p>
       </div>
     </div>
   );
