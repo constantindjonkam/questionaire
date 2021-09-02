ace79b7d9723dba2c117200dca34ca512a1c7c51
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-09-02 02:48:12 -0400

    created result card

---
+++ src/components/ResultCard.tsx
@@ -0,0 +1,21 @@
+import "./styles/resultCard.scss";
+
+export interface ResultCardProps {
+  image: string;
+  title: string;
+  value: number;
+}
+
+const ResultCard: React.FC<ResultCardProps> = ({ image, title, value }) => {
+  return (
+    <div className="resultCard">
+      <img src={image} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p className="resultCard__value">{value}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ResultCard;
