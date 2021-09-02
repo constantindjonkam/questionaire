7e94cea3d34c0f91a295ce9261ef499f39020584
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-09-02 16:52:49 -0400

    refactored firebase by moving all unrelated funtions

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,28 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  where,
-  query,
-  serverTimestamp,
-} from "firebase/firestore/lite";
-
-import { orderByDate } from "./utils/dateFilter";
-import { pointsCalculator } from "./utils/pointsCalulator";
-import { ResultProp } from "./components/TableBody";
-
-export interface UserResult {
-  avoidance: number;
-  intrusions: number;
-  cognitions: number;
-  hypervigilance: number;
-  result: ResultProp[];
-  total: number;
-  user: string;
-  created: { seconds: number; nanoseconds: number };
-}
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDoPVoF4dQYw9HICIBT6YMrejiyUMpnYzQ",
@@ -36,54 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-async function getResults() {
-  const resultsCol = collection(db, "results");
-  try {
-    const resultSnapshot = await getDocs(resultsCol);
-    const resultList = resultSnapshot.docs.map((doc) => doc.data());
-    return resultList;
-  } catch (error) {
-    console.log("Could not get results from db");
-  }
-}
-
-async function getUserResults(user: string) {
-  const results = [] as UserResult[];
-  const q = query(collection(db, "results"), where("user", "==", user));
-
-  try {
-    const resultSnapshot = await getDocs(q);
-    resultSnapshot.forEach((doc) => results.push(doc.data() as UserResult));
-  } catch (error) {
-    console.log("Could not get user results from db");
-  }
-
-  return orderByDate(results);
-}
-
-async function addResult(user: string, result: ResultProp[]) {
-  const total = result.reduce((a, c) => Number(c.value) + a, 0);
-  const avoidance = pointsCalculator(result, 1, 5);
-  const intrusions = pointsCalculator(result, 6, 7);
-  const cognitions = pointsCalculator(result, 8, 14);
-  const hypervigilance = pointsCalculator(result, 15, 20);
-
-  try {
-    await addDoc(collection(db, "results"), {
-      user,
-      result: result,
-      avoidance,
-      intrusions,
-      cognitions,
-      hypervigilance,
-      total,
-      created: serverTimestamp(),
-    });
-  } catch (error) {
-    console.log("Could not add result to db");
-  }
-}
-
-export { getResults, addResult, getUserResults };
+export const db = getFirestore(app);
