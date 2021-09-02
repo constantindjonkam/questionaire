cb4a9195e9f3d4dcb6e0fc6a0fa9db7c8edfcc64
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-09-02 15:54:09 -0400

    added scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.scss";
+import ScrollToTop from "./components/ScrollToTop";
 import Question from "./pages/Question";
 import Result from "./pages/Result";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop />
         <Switch>
           <Route path="/result/:id" component={Result} />
           <Route path="/" component={Question} />
