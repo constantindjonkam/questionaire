b6773cca48da62293769d825d33b103158e41c53
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-09-02 16:53:15 -0400

    moved score comparator to determiners.ts

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,37 +1,27 @@
 import { Line } from "react-chartjs-2";
-import { UserResult } from "../firebase";
 
+import { UserResult } from "../services/userResults";
+import { scoreComparator } from "../utils/determiners";
 import { randomColors } from "../utils/randomColor";
 
 export interface LineChartProps {
-  result: UserResult[];
+  results: UserResult[];
 }
 
-const LineChart: React.FC<LineChartProps> = ({ result }) => {
-  const scoreComparator = () => {
-    if (result.length < 2) return;
-
-    if (result[0].total > result[1].total)
-      return "You are experiencing an increased in stress. Please consult a doctor immidiately.";
-    else if (result[0].total < result[1].total)
-      return "You are starting to feel better. Please keep up!";
-
-    return "No improvement on your health detected. Please consult a doctor immidiately to check if everything is alright.";
-  };
-
+const LineChart: React.FC<LineChartProps> = ({ results }) => {
   return (
     <div className="lineChart">
       <Line
         data={{
-          labels: result
+          labels: results
             .map((res, i) => new Date(res.created.seconds * 1000).toLocaleDateString())
             .reverse(),
           datasets: [
             {
               label: "Survey result history",
-              data: result.map((res) => res.total).reverse(),
-              backgroundColor: randomColors(result.map((res) => res.total).length),
-              borderColor: randomColors(result.map((res) => res.total).length),
+              data: results.map((res) => res.total).reverse(),
+              backgroundColor: randomColors(results.map((res) => res.total).length),
+              borderColor: randomColors(results.map((res) => res.total).length),
               borderWidth: 1,
             },
           ],
@@ -43,7 +33,7 @@ const LineChart: React.FC<LineChartProps> = ({ result }) => {
         className="lineChart__message"
         style={{ marginTop: 40, color: "tomato", textAlign: "center" }}
       >
-        {scoreComparator()}
+        {scoreComparator(results)}
       </p>
     </div>
   );
