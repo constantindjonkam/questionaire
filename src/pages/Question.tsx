cd649e012de8a72ea02e29f6463916bd242d732d
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-09-01 21:12:15 -0400

    added heading, instructions and survey table

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,9 +1,21 @@
+import QuestionaireTable from "../components/QuestionaireTable";
+import "./styles/question.scss";
+
 export interface QuestionProps {}
 
 const Question: React.FC<QuestionProps> = () => {
   return (
-    <div>
-      <p>Question</p>
+    <div className="question">
+      <h2>Quick Survey</h2>
+      <p className="question__instructions">
+        <span>Instructions: </span>
+        Below is a list of problems that people sometimes have in response to a very stressful
+        experience. Please read each problem carefully and then circle one of the numbers to the
+        right to indicate how much you have been bothered by that problem{" "}
+        <span>in the past month.</span>
+      </p>
+
+      <QuestionaireTable />
     </div>
   );
 };
