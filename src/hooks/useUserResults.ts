2d5d1662e67aba84c0a1c0f7a6c31e37ee7e284c
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-09-02 17:52:43 -0400

    removed useParams and parsed it as prop

--- src/hooks/useUserResults.ts
+++ src/hooks/useUserResults.ts
@@ -1,19 +1,16 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 
 import { getUserResults, UserResult } from "../services/userResults";
 
-const useUserResults = () => {
+const useUserResults = (params: object) => {
   const [results, setResults] = useState<UserResult[]>([]);
   const latestResult = results[0];
-  const params = useParams();
 
   useEffect(() => {
-    (async () => {
-      const res = await getUserResults((params as { id: string }).id);
-      setResults(res);
-    })();
-  }, [results, params]);
+    getUserResults((params as { id: string }).id)
+      .then((res) => setResults(res)) //orderByDate(
+      .catch(() => console.log("could not get user results"));
+  }, [params]);
 
   return { results, latestResult };
 };
