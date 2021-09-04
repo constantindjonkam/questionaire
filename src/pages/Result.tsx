27c814d1d65a74377036a6ca19febbcc6a7f8f70
Constantin Djonkam <fokouaconstantin@gmail.com>
2021-09-03 21:09:40 -0400

    removed params implementation and added loader indicator

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,9 +1,8 @@
-import { useParams } from "react-router-dom";
-
 import useUserResults from "../hooks/useUserResults";
 import Header from "../components/Header";
 import LineChart from "../components/LineChart";
 import ResultCard from "../components/ResultCard";
+import Loader from "../components/Loader";
 import { colorDeterminer, resultMessageDeterminer, severityDeterminer } from "../utils/determiners";
 import hypertension from "../assets/hypertension.jpg";
 import cognition from "../assets/cognition.jpg";
@@ -12,10 +11,11 @@ import avoidance from "../assets/avoidance.jpg";
 import "./styles/result.scss";
 
 const Result: React.FC = () => {
-  const params = useParams();
-  const { results, latestResult } = useUserResults(params);
+  const { results, latestResult } = useUserResults();
 
-  if (!results.length)
+  if (results === null) return <Loader />;
+
+  if (!results.length || !latestResult)
     return (
       <p>
         User data could not be determined. Make sure you have submitted a survey with the username
